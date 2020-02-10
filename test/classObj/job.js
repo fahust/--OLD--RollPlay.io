ca45b07307fefccdcb1496ec569ad99290d1bf2b
unknown <k.dellova@coccinet.com>
2020-02-10 18:03:14 +0100

    job

--- test/classObj/job.js
+++ test/classObj/job.js
@@ -17,12 +17,18 @@ class Job{
     job.push('Charpentier');//Bonus de construction de batiment
     job.push('Diplomate');//
     job.push('Forgeron');//bonus de forge d'arme
-    job.push('Garde');//protège les pnj et joueur
     job.push('Serrurier');//vitale pour ouvrir certaines zone et les fermer
     job.push('Alchimiste');//bonus de fabrication de potion
     job.push('Voleur');//rajouter action vol
-    job.push('Explorateur');
-    job.push('Explorateur');
-    job.push('Explorateur');
+    job.push('Sorcier');//créer des skill et les apprend au autres joueur
+
+    job.push('bandit');//
+    job.push('Dark Knight');//
+    job.push('Maitre des démons');//fais apparaitre des démons//avoir monter les échellon pour en arriver si haut
+
+    job.push('garde');//protège les pnj et joueur
+    job.push('Dark Knight');//
+    job.push('Maitre des démons');//fais apparaitre des démons//avoir monter les échellon pour en arriver si haut
+
   }
 }
